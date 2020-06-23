ccfc81a10c397841ead43cf08c640f3e61d71838
OfficiallySp777 <shane@192.168.1.223>
2020-06-23 17:43:10 +0100

    init

---
+++ src/main/java/com/officiallysp/aether/block/SkyrootLeavesBlock.java
@@ -0,0 +1,50 @@
+
+package com.officiallysp.aether.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import com.officiallysp.aether.AetherRebornModElements;
+
+@AetherRebornModElements.ModElement.Tag
+public class SkyrootLeavesBlock extends AetherRebornModElements.ModElement {
+	@ObjectHolder("aether_reborn:skyroot_leaves")
+	public static final Block block = null;
+	public SkyrootLeavesBlock(AetherRebornModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends LeavesBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.LEAVES).sound(SoundType.PLANT).hardnessAndResistance(1f, 10f).lightValue(0).notSolid());
+			setRegistryName("skyroot_leaves");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
