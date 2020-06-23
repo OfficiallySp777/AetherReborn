684f4a13e239152f7f6e352b7bb219b103a61046
OfficiallySp777 <shane@192.168.1.223>
2020-06-23 19:41:46 +0100

    Tools

---
+++ src/main/java/com/officiallysp/aether/item/SkyrootSwordItem.java
@@ -0,0 +1,51 @@
+
+package com.officiallysp.aether.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import com.officiallysp.aether.AetherRebornModElements;
+
+@AetherRebornModElements.ModElement.Tag
+public class SkyrootSwordItem extends AetherRebornModElements.ModElement {
+	@ObjectHolder("aether_reborn:skyroot_sword")
+	public static final Item block = null;
+	public SkyrootSwordItem(AetherRebornModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 250;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 0f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(ItemGroup.COMBAT)) {
+		}.setRegistryName("skyroot_sword"));
+	}
+}
