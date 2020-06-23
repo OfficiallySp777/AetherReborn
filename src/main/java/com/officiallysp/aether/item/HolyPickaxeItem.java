08e232596a2059f305501db24818bf1d9c48a606
OfficiallySp777 <shane@192.168.1.223>
2020-06-24 00:46:25 +0100

    Incubator

--- src/main/java/com/officiallysp/aether/item/HolyPickaxeItem.java
+++ src/main/java/com/officiallysp/aether/item/HolyPickaxeItem.java
@@ -3,12 +3,18 @@ package com.officiallysp.aether.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
 
+import com.officiallysp.aether.procedures.HolyPickaxeBlockDestroyedWithToolProcedure;
 import com.officiallysp.aether.AetherrebornModElements;
 
 @AetherrebornModElements.ModElement.Tag
@@ -46,6 +52,19 @@ public class HolyPickaxeItem extends AetherrebornModElements.ModElement {
 				return Ingredient.EMPTY;
 			}
 		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public boolean onBlockDestroyed(ItemStack itemstack, World world, BlockState bl, BlockPos pos, LivingEntity entity) {
+				boolean retval = super.onBlockDestroyed(itemstack, world, bl, pos, entity);
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+				{
+					java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+					$_dependencies.put("entity", entity);
+					HolyPickaxeBlockDestroyedWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
 		}.setRegistryName("holy_pickaxe"));
 	}
 }
