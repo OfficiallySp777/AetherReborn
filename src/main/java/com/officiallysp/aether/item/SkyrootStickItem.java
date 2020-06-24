f589ede8c6ba2b0bb2d4f8a65558d0ea9638ebc0
OfficiallySp777 <shane@192.168.1.223>
2020-06-24 01:12:39 +0100

    Torch

---
+++ src/main/java/com/officiallysp/aether/item/SkyrootStickItem.java
@@ -0,0 +1,46 @@
+
+package com.officiallysp.aether.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import com.officiallysp.aether.itemgroup.AetherTabItemGroup;
+import com.officiallysp.aether.AetherrebornModElements;
+
+@AetherrebornModElements.ModElement.Tag
+public class SkyrootStickItem extends AetherrebornModElements.ModElement {
+	@ObjectHolder("aetherreborn:skyroot_stick")
+	public static final Item block = null;
+	public SkyrootStickItem(AetherrebornModElements instance) {
+		super(instance, 42);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(AetherTabItemGroup.tab).maxStackSize(64));
+			setRegistryName("skyroot_stick");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
